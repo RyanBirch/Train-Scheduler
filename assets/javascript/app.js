c955f12f2f70fe814f5fdcfecb712d1efb60a7e0
Ryan Birch <ryan.birch@gmail.com>
2019-04-21 22:13:48 -0400

    added html

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBE7YAbOPWGfvflwyUoQ3FWzJQKyLSoOn0",
+  authDomain: "train-scheduler-e9e85.firebaseapp.com",
+  databaseURL: "https://train-scheduler-e9e85.firebaseio.com",
+  projectId: "train-scheduler-e9e85",
+  storageBucket: "train-scheduler-e9e85.appspot.com",
+  messagingSenderId: "592072680965"
+};
+firebase.initializeApp(config);
+
+$('submit').on('click', function() {
+  
+})
